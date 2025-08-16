fc612b3f282ebad71f26d7eb4b732f592531d14a
Sean Levin <sean.levin@gmail.com>
2025-08-16 13:31:26 -0700

    Fix Tests (#11)
    * migrate from pods to spm tests
    
    * disable test for checking if resource can be updated before adding extent
    
    - wasn't failing before because xcodebuild wasn't running these quickspec tests
    - doesn't look like it is supported in code
    
    * migrate to standard xctest from quick/nimble
    
    ---------
    
    Co-authored-by: Sean Levin <slevin@yahooinc.com>

--- Package.swift
+++ Package.swift
@@ -17,7 +17,6 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,6 +24,10 @@ let package = Package(
         .target(
             name: "BGSwift",
             path: "BGSwift/Classes"),
+        .testTarget(
+            name: "BGSwiftTests",
+            dependencies: ["BGSwift"],
+            path: "Example/Tests"),
     ],
     swiftLanguageVersions: [.v5]
 )
